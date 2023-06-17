2e7e0c4648d05e790638477c62483a8f2aff3548
nozomi-iida <iida19990106@gmail.com>
2023-06-17 16:14:14 +1000

    feat: add shadow animation in header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,31 @@
+"use client";
+
 import { pagesPath } from "@/lib/pathpida/$path";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export function Header() {
+  const [showShadow, setShowShadow] = useState(false);
+
+  useEffect(() => {
+    const addShadow = () => {
+      if (window.pageYOffset === 0) {
+        setShowShadow(false);
+      } else {
+        setShowShadow(true);
+      }
+    };
+
+    window.addEventListener("scroll", addShadow);
+    return () => window.removeEventListener("scroll", addShadow);
+  }, [setShowShadow]);
+
   return (
-    <header className="fixed top-0 w-full z-header bg-white">
+    <header
+      className={`fixed top-0 w-full z-header bg-white transition-shadow ${
+        showShadow && "shadow-md"
+      }`}
+    >
       <div className="max-w-5xl mx-auto">
         <div className="flex m-auto px-4 justify-between">
           <Link href={pagesPath.$url()}>
