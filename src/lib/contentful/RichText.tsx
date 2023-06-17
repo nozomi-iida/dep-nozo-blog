6ee21474a9bfa20d1fee8ff71ec91df93cf9b428
nozomi-iida <iida19990106@gmail.com>
2023-06-17 18:20:32 +1000

    feat: customize rich text

--- src/lib/contentful/RichText.tsx
+++ src/lib/contentful/RichText.tsx
@@ -1,5 +1,11 @@
-import { Document as RichTextDocument } from "@contentful/rich-text-types";
-import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import {
+  BLOCKS,
+  Document as RichTextDocument,
+} from "@contentful/rich-text-types";
+import {
+  documentToReactComponents,
+  RenderNode,
+} from "@contentful/rich-text-react-renderer";
 
 type RichTextProps = {
   document: RichTextDocument | null;
@@ -9,8 +15,16 @@ function RichText({ document }: RichTextProps) {
   if (!document) {
     return null;
   }
+  const renderNode: RenderNode = {
+    [BLOCKS.HEADING_1]: (_, children) => (
+      <h1 className="text-2xl font-semibold">{children}</h1>
+    ),
+    [BLOCKS.HEADING_2]: (_, children) => (
+      <h2 className="text-xl font-semibold">{children}</h2>
+    ),
+  };
 
-  return <>{documentToReactComponents(document)}</>;
+  return <>{documentToReactComponents(document, { renderNode })}</>;
 }
 
 export default RichText;
